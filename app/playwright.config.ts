0eb60c9bae4b845a6a6c29d700dec602cd3a30f3
csiziszebasztian <tanulas88@gmail.com>
2022-10-01 08:30:14 +0200

    add dev url

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -33,7 +33,7 @@ const config: PlaywrightTestConfig = {
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: 'https://localhost/',
+    baseURL: 'https://localhost:4443/',
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
